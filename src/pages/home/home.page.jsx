66c1ea7aa4e49524e81c41226c5c8c623bd5197e
yacout <yacoutlouryagli@gmail.com>
2023-01-30 12:29:00 +0100

    create navigation bar

---
+++ src/pages/home/home.page.jsx
@@ -0,0 +1,12 @@
+import "./home.style.css";
+
+
+const Home = () => {
+    return (
+        <div>
+            This is Home
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/navigation/navigation.page.jsx
@@ -0,0 +1,19 @@
+import "./navigation.style.css";
+
+const Navigation =()=> {
+    return (
+    <div className="navigation-bar">
+        <div className="nav-left">
+            <span> Logo </span>
+        </div>
+        <div className="nav-right">
+                <span> Collection </span>
+                <span> About </span>
+                <span> Contact </span>
+            </div>
+    </div>
+        
+    )
+}
+
+export default Navigation;
