f02f7a0185609e533a74cb539212a30e11f6e8b9
yacout <yacoutlouryagli@gmail.com>
2023-01-31 15:14:56 +0100

    this is just a test for commiting 	1changed button 	2 changed navigatip

--- src/pages/navigation/navigation.page.jsx
+++ src/pages/navigation/navigation.page.jsx
@@ -1,13 +1,14 @@
 import "./navigation.style.css";
 
-const Navigation =()=> {
+const Navigation =()=> { 
     return (
     <div className="navigation-bar">
         <div className="nav-left">
             <span> Logo </span>
         </div>
         <div className="nav-right">
-                <span> Collection </span>
+
+                <span > Collection </span>
                 <span> About </span>
                 <span> Contact </span>
             </div>
