db3e35360559c77cd59c2d3abb6eba0717bf78ba
yacout <yacoutlouryagli@gmail.com>
2023-02-04 16:28:08 +0100

    new cards style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navigation from './pages/navigation/navigation.page.jsx';
 import Collection from './pages/collection/collection.page';
+import Contact from './pages/contact/contact.page.jsx';
 import {Routes, Route} from 'react-router-dom';
 
 
@@ -12,8 +13,8 @@ function App() {
     <Routes>
         <Route   path='/'  element={<Navigation />}>
           {/* <Route index  element={<Home />} /> */}
-          {/* <Route path='/shop'  element={<Shop />} />
-          <Route path='/auth'  element={<Authentication />} /> */}
+          {/* {/<Route path='/shop'  element={<Shop />} /> */}
+          <Route path='/contact'  element={<Contact />} /> 
           <Route path='/collection'  element={<Collection />} />
       </Route>
         
--- src/pages/navigation/navigation.page.jsx
+++ src/pages/navigation/navigation.page.jsx
@@ -7,13 +7,13 @@ const Navigation =()=> {
         <Fragment>
     <div className="navigation-bar">
         <div className="nav-left">
-            <span> Logo </span>
+        <Link to="/"><span> Logo </span></Link> 
         </div>
         <div className="nav-right">
             <Link to="/collection"><span > Collection </span></Link>
                 
                 <span> About </span>
-                <span> Contact </span>
+              <Link to="/contact" ><span> Contact </span></Link>
             </div>
             
     </div>
