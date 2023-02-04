feca25c5b535a9c229aed49483b0fa45dd51efb3
amine <atlagh.amine@gmail.com>
2023-02-04 12:56:01 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navigation from './pages/navigation/navigation.page.jsx';
 import Collection from './pages/collection/collection.page';
+import {Routes, Route} from 'react-router-dom';
 
 
 
@@ -8,15 +9,15 @@ import Collection from './pages/collection/collection.page';
 
 function App() {
   return (
-    <div>
-    
-   <Navigation/>
-   <Collection/>
-  
-   
-
-    
-    </div>
+    <Routes>
+        <Route   path='/'  element={<Navigation />}>
+          {/* <Route index  element={<Home />} /> */}
+          {/* <Route path='/shop'  element={<Shop />} />
+          <Route path='/auth'  element={<Authentication />} /> */}
+          <Route path='/collection'  element={<Collection />} />
+      </Route>
+        
+    </Routes>
     
       
   );
--- src/pages/navigation/navigation.page.jsx
+++ src/pages/navigation/navigation.page.jsx
@@ -1,18 +1,24 @@
 import "./navigation.style.css";
+import {Outlet, Link} from 'react-router-dom';
+import { Fragment } from "react";
 
 const Navigation =()=> { 
     return (
+        <Fragment>
     <div className="navigation-bar">
         <div className="nav-left">
             <span> Logo </span>
         </div>
         <div className="nav-right">
-
-                <span > Collection </span>
+            <Link to="/collection"><span > Collection </span></Link>
+                
                 <span> About </span>
                 <span> Contact </span>
             </div>
+            
     </div>
+    <Outlet/>
+    </Fragment>
         
     )
 } ;
